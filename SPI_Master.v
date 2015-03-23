b424527b441cb9b1cec20d7df7f0f93aa1b41768
kennedys <plan9os2000@gmail.com>
2015-03-23 15:38:56 -0700

    State Machine
    4 State machine: Idle, Wait Half, Transfer

--- SPI_Master.v
+++ SPI_Master.v
@@ -1,71 +1,97 @@
-module SPI_Master( input clk_50M,
-						 input reset,
-						 input MISO_bit,
-						 input [2:0] address,
-						 output reg SCLK,
-						 output reg MOSI_bit,
-						 output reg SS,
-						 output reg [6:0] SevSeg
-						);
-						
-	reg edgeTrigger; //1 if rising, 0 if falling
-	reg loadReg;
-	reg [7:0] data;
-	reg [5:0] bitCount;
-	wire clk_16Hz;
-	wire clk_8Hz;
-	
-   clkDivider_16Hz u0( clk_50M, clk_16Hz, reset);
-	clkDivider_8Hz u1( clk_50M, clk_8Hz, reset);
-	
-	initial begin
-		edgeTrigger = 0;
-		loadReg = 0;
-		data = 8'b00000000;
-		bitCount = 5'b00000;
-		SevSeg = 8'b00000000;
-	end
-	
-	//This block will send data to the slave on rising edge
-	always @ (posedge clk_16Hz) begin
-			if(edgeTrigger && !SS) begin
-				MOSI_bit <= data[7];
-			end
-			else if (!edgeTrigger && !SS) begin
-				data <={data[6:0], MISO_bit};
-				bitCount <= bitCount + 1;
-			end else if (loadReg) begin
-				data <= {5'b00000, address};
-				bitCount <= 5'b00000;
-			end 
-		
-			if (bitCount == 16) begin
-				bitCount <= 5'b00000;
-			end
-			
-			
-			edgeTrigger <= ~edgeTrigger;
-	end
-	
-	//Always block will drive a 
-	always @ (*) begin
-			
-			if(bitCount == 16) begin
-				SevSeg <= ~data[6:0];
-				SS <= 0;
-				SCLK <= 0;
-				loadReg <= 0;
-			end else if(bitCount == 0 || !reset) begin
-				loadReg <= 1;
-			end
-			else begin
-				loadReg <= 0;
-				SS <= 1;
-				SCLK <= clk_8Hz;
-			end
-			
-			
-	end
-	
-	
+module spi #(parameter CLK_DIV = 2)(
+        input clk,
+        input rst,
+        input miso,
+        output mosi,
+        output sck,
+        input start,
+        input[7:0] data_in,
+        output[7:0] data_out,
+        output busy,
+        output new_data
+    );
+     
+    localparam STATE_SIZE = 2;
+    localparam IDLE = 2'd0,
+            WAIT_HALF = 2'd1,
+            TRANSFER = 2'd2;
+     
+    reg [STATE_SIZE-1:0] state_d, state_q;
+     
+    reg [7:0] data_d, data_q;
+    reg [CLK_DIV-1:0] sck_d, sck_q;
+    reg mosi_d, mosi_q;
+    reg [2:0] ctr_d, ctr_q;
+    reg new_data_d, new_data_q;
+    reg [7:0] data_out_d, data_out_q;
+     
+    assign mosi = mosi_q;
+    assign sck = (~sck_q[CLK_DIV-1]) & (state_q == TRANSFER);
+    assign busy = state_q != IDLE;
+    assign data_out = data_out_q;
+    assign new_data = new_data_q;
+     
+    always @(*) begin
+        sck_d = sck_q;
+        data_d = data_q;
+        mosi_d = mosi_q;
+        ctr_d = ctr_q;
+        new_data_d = 1'b0;
+        data_out_d = data_out_q;
+        state_d = state_q;
+         
+        case (state_q)
+            IDLE: begin
+                sck_d = 4'b0;
+                ctr_d = 3'b0;
+                if (start == 1'b1) begin
+                    data_d = data_in;
+                    state_d = WAIT_HALF;
+                end
+            end
+            WAIT_HALF: begin
+                sck_d = sck_q + 1'b1;
+                if (sck_q == {CLK_DIV-1{1'b1}}) begin
+                    sck_d = 1'b0;
+                    state_d = TRANSFER;
+                end
+            end
+            TRANSFER: begin
+                sck_d = sck_q + 1'b1;
+                if (sck_q == 4'b0000) begin
+                    mosi_d = data_q[7];
+                end else if (sck_q == {CLK_DIV-1{1'b1}}) begin
+                    data_d = {data_q[6:0], miso};
+                end else if (sck_q == {CLK_DIV{1'b1}}) begin
+                    ctr_d = ctr_q + 1'b1;
+                    if (ctr_q == 3'b111) begin
+                        state_d = IDLE;
+                        data_out_d = data_q;
+                        new_data_d = 1'b1;
+                    end
+                end
+            end
+        endcase
+    end
+     
+    always @(posedge clk) begin
+        if (rst) begin
+            ctr_q <= 3'b0;
+            data_q <= 8'b0;
+            sck_q <= 4'b0;
+            mosi_q <= 1'b0;
+            state_q <= IDLE;
+            data_out_q <= 8'b0;
+            new_data_q <= 1'b0;
+        end else begin
+            ctr_q <= ctr_d;
+            data_q <= data_d;
+            sck_q <= sck_d;
+            mosi_q <= mosi_d;
+            state_q <= state_d;
+            data_out_q <= data_out_d;
+            new_data_q <= new_data_d;
+        end
+    end
+     
 endmodule
